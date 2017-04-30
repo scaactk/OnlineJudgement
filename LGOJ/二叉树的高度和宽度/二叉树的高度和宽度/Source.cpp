9d8d66a8020294f90b316a936b67819887980be5
Scaactk <tjut_ljx@yeah.net>
2017-04-30 09:59:42 +0800

    求二叉树的宽度和深度

--- LGOJ/二叉树的高度和宽度/二叉树的高度和宽度/Source.cpp
+++ LGOJ/二叉树的高度和宽度/二叉树的高度和宽度/Source.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 using namespace std;
+int count = 0;//用于记录当前层级的节点个数
 struct Node {
 	char data;
 	Node* left_child;
@@ -35,11 +36,51 @@ int get_deepth(Node* T) {
 	dp = (left > right ? left : right) + 1;
 	return dp;
 }
+int level_count(Node* T,int level)
+{
+	if (!T || level < 1) {
+		return 0;
+	}
+	else if (level == 1) {
+		count++;//这里是通过cpp作用域特性实现，可能会存在问题
+	}
+	else {
+		level_count(T->left_child, level - 1);
+		level_count(T->right_child, level - 1);
+	}
+	return count;
+}
+void bubble_sort(int* a,int dp)
+{
+	for (int i = 0; i < dp; i++) {
+		for (int j = i + 1; j < dp; j++) {
+			if (a[i] < a[j]) {
+				int temp;
+				temp = a[i];
+				a[i] = a[j];
+				a[j] = temp;
+			}
+		}
+	}
+}
+int get_width(Node* T, int level)
+{
+	int stack[level+5];
+	for (int i = 1; i <= level; i++) {
+		count = 0;
+		stack[i - 1] = level_count(T, i);
+	}
+	bubble_sort(stack,level);
+	return stack[0];
+}
 int main()
 {
 	Node* T;
 	T = create_Bitree();
-	cout << get_deepth(T) << endl;
+	int dp = get_deepth(T);
+	cout << dp <<" ";
+	int wd = get_width(T,dp);
+	cout<< wd <<endl;
 	system("pause");
 	return 0;
 }
