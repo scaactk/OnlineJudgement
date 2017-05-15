3bcccc5df763ba8423cca46f627dad5e1483c907
Scaactk <tjut_ljx@yeah.net>
2017-05-15 19:12:14 +0800

    无向图的遍历

--- LGOJ/无向图的遍历/无向图的遍历/Source.cpp
+++ LGOJ/无向图的遍历/无向图的遍历/Source.cpp
@@ -6,7 +6,7 @@ struct Graph {
 	int n;
 	int e;
 };
-void create_graph(Graph* G, int n, int e, int* ver, int** side)
+void create_graph(Graph* G, int n, int e, int* ver, int side[][2])
 {
 	G->n = n;
 	G->e = e;
@@ -23,6 +23,18 @@ void create_graph(Graph* G, int n, int e, int* ver, int** side)
 		G->edges[side[i][1]][side[i][0]] = 1;
 	}
 }
+void print_graph(Graph* G)
+{
+	for (int i = 0; i < G->n; i++)
+	{
+		cout << G->vertexs[i];
+		for (int j = 0; j < G->e; j++)
+		{
+			cout << " " << G->edges[j];
+		}
+		cout << endl;
+	}
+}
 void depth_first_search()
 {
 
@@ -36,12 +48,17 @@ int main()
 	int n, m;
 	cin >> n >> m;
 	int ver[100];
-	int side[100*100][2];
+	int side[10000][2];
 	for (int i = 0; i < n; i++) {
 		ver[i] = i + 1;
 	}
 	for (int i = 0; i < m; i++) {
 		cin >> side[i][0] >> side[i][1];
+		cout << side[i][0] << side[i][1] << endl;
 	}
 	Graph* G = new Graph();
+	create_graph(G, n, m, ver, side);
+	print_graph(G);
+	system("pause");
+	return 0;
 }
